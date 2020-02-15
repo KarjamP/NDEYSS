28654cfdb42989e41ec50e14170cb4e75f7d4648
Richard James "KarjamP" De Goede <KarjamP@GMail.com>
2020-02-15 23:28:32 +0200

    WIP code refactoring...

--- GLSL/NEL Prototype I.glsl
+++ GLSL/NEL Prototype I.glsl
@@ -11,7 +11,7 @@ uniform vec2 resolution;
 //NEL Prototype: The end product should eventually result in a shader that conjures forth and shows
 //a single cube entirely through the power of emotions.
 //
-//Incomplete; One more step: Make the reality talk NTSC...
+//Incomplete; Close, but not quite; busy rethinking implementation...
 
 //Normally, GLSL does not allow for persistance of information. But, it allows for a very keen
 //simulation of a hardware system designed to work with pure analogue signals...
@@ -29,10 +29,6 @@ uniform vec2 resolution;
 
 //some of the code. Counts as Fair Use due to this prototype being "transformative"
 //(In other words, it has an identity independant from the work it's based on).
-
-float sinSig = abs(sin(time*59.95));
-//Mimics NTSC signal; A perfect fit, if you ask me; it allows for image generation
-//via analogue signal decoding, alone.
 
 
 
@@ -62,99 +58,21 @@ float rand(vec3 n){
  
 
 float rand(float n){
-
-	return( fract( sin (( ( hash12( vec2(n,n*3.2)) + hash12(vec2(n,n*3.2)) * 1e2 + hash12(vec2(n,n*3.2)) * 1e4) * 1e-2 )) * 1e5));
+	
+	return( fract( sin( ( ( hash12( vec2(n,n*3.2)) + hash12(vec2(n,n*3.2)) * 1e2 + hash12(vec2(n,n*3.2)) * 1e4) * 1e-2 )) * 1e5));
 
 }
 
-//************************//
-//The folowing is code copied from https://github.com/libretro/glsl-shaders/blob/master/misc/ntsc-colors.glsl .
-//
-//Ambigous license; the placement of a GPL license within the folder of a separate shader suggests that the
-//individual shaders each have their own licenses.
-//
-//"Stealing" this code is a lot more easier than finding a way to decode NTSC, and then writing new
-//code from scratch.
-//
-//The use of shader code featured under the LibRetro project should be Fair Use (and thus, allowed);
-//no attempts of modification to that code is made, and the relevant code can easily be replaced with
-//another. However, things vital for reproduction of programming code transformative in nature
-//is not coverable by copyright (as proven within past lawsuits involving reproduction of a computer
-//BIOS.
-//************************//
 
-vec3 DecodeGamma(vec3 color, float gamma)
-{
-    color = clamp(color, 0.0, 1.0);
-    color.r = (color.r <= 0.00313066844250063) ?
-    color.r * 12.92 : 1.055 * pow(color.r, 1.0 / gamma) - 0.055;
-    color.g = (color.g <= 0.00313066844250063) ?
-    color.g * 12.92 : 1.055 * pow(color.g, 1.0 / gamma) - 0.055;
-    color.b = (color.b <= 0.00313066844250063) ?
-    color.b * 12.92 : 1.055 * pow(color.b, 1.0 / gamma) - 0.055;
 
-    return color;
-}
 
-vec3 RGBtoXYZ(vec3 RGB)
-  {
-      mat3 m = mat3(
-      0.6068909, 0.1735011, 0.2003480,
-      0.2989164, 0.5865990, 0.1144845,
-      0.0000000, 0.0660957, 1.1162243);
-  
-    return RGB * m;
-  }
-
-vec3 XYZtoSRGB(vec3 XYZ)
-{
-    mat3 m = mat3(
-    3.2404542,-1.5371385,-0.4985314,
-   -0.9692660, 1.8760108, 0.0415560,
-    0.0556434,-0.2040259, 1.0572252);
-
-    return XYZ * m;
-  }
-
-vec3 XYZtoRGB(vec3 XYZ)
-  {
-      mat3 m = mat3(
-      1.9099961, -0.5324542, -0.2882091,
-     -0.9846663,  1.9991710, -0.0283082,
-      0.0583056, -0.1183781,  0.8975535);
-  
-    return XYZ * m;
-}
-
-// conversion from NTSC RGB Reference White D65 ( color space used by NA/Japan TV's ) to XYZ
-vec3 NTSC(vec3 c)
- {
-     vec3 v = vec3(pow(c.r, 2.2), pow(c.g, 2.2), pow(c.b, 2.2)); //Inverse Companding
-     return RGBtoXYZ(v);
- }
-
-// conversion from XYZ to sRGB Reference White D65 ( color space used by windows ) 
-vec3 sRGB(vec3 c)
- {
-     vec3 v = XYZtoSRGB(c);
-     v = DecodeGamma(v, 2.4); //Companding
- 
-     return v;
- }
-
-// NTSC RGB to sRGB
-vec3 NTSCtoSRGB( vec3 c )
- { 
-     return sRGB(NTSC( c )); 
- }
-
-//************************//
 
 
 
 void main( void ) {
 	vec2 position = ( resolution.xy /  gl_FragCoord.xy );
 	float combinedPosition = (position.x + position.y);
+	float correctedTime = 1.0 / fract (time / 10000.0);
 
 	
 	//This variable holds the starting point to the simulation; the point is to constantly
@@ -164,71 +82,41 @@ void main( void ) {
 	//
 	//(Use the rule of symbolism within code to carry details not easily carried through
 	// pure code; the emotions behind the code knows what to do...)
+	
+	
+	vec3 nel = vec3( 3.0, 5.0, 9.0);
+	
+	nel.r = mix (nel.r, rand(nel.r/time),1.0);
+	nel.b = mix (nel.b, rand(nel.b/time),1.0);
+	nel.g = mix (nel.g, rand(nel.g/time),1.0);
+	
+	nel /= time;
+	
+	nel = normalize(nel);
+	
+	//Reliable, but the nature of this implementation forces one to work in 
+	//terms of ratio, not direct variables.
 	//
-	// 3.0 = rotating
-	// 5.0 = cube
-	// 9.0 = spin
-	vec3 color = vec3(3.0,5.0,9.0);
+	//Close enough. Was struggling with the "direct variable" approach
+	//in either case. Now, it's just a matter of discovering how to literally
+	//program in ratios...
+	
 
 	
-	//Noise allows for code to remain non-rigid enough for the rules
-	//of time to be properly simulated...
-	//(combinedPosition makes certain that the noise varies between each pixel; this is important to, you know,
-	//actually create noise...)
-	//dcolor *= (rand(color * combinedPosition),rand(color * combinedPosition),rand(color * combinedPosition)) * time;
-	color *= (rand(color),rand(color),rand(color)) * time;
-	//We want a true elctrostatic noise for an output...
-	color = normalize (color);
-
 	
-	// Some parts of the signal seep too low for our needs;
-	// let's clamp it to a range which allows for near
-	// unnoticeable pulse...
-	//color = (color * ( 1.0 - 0.9)) + 0.9;
-	// 1.0 - max number
-	// 0.9 - min number
-
-	
-	//For reference...
-	//color.r += sin( position.x * cos( time / 15.0 ) * 80.0 ) + cos( position.y * cos( time / 15.0 ) * 10.0 );
-
-	//color.g += sin( position.y * sin( time / 10.0 ) * 40.0 ) + cos( position.x * sin( time / 25.0 ) * 40.0 );
-
-	//color.b += sin( position.x * sin( time / 5.0 ) * 10.0 )  + sin( position.y * sin( time / 35.0 ) * 80.0 );
-
- 
-
-		
-
-	//This variable is meant to represent the "Pure Intent" of the reality; this allows for
-	//the reality to communicate with  the shader, thereby allowing us to see what's within
-	//it.
-	vec3 color2;
-
-	
-	//Generating pseudorandom noise based on the on-screen position.
-	//
-	//Going to need to make sure "time" doesn't get too big;
-	//that causes problems within the pseudorandom noise.
-	//
-	//The only glitch so far with this approach seems to be the tendency
-	//for the view to "flicker"; since this mimics how real life
-	//faulty monitors work, however, it's still viable for our own purposes...
-	color2.r = rand( ( 1.0 / fract (time / 10000.0)) * combinedPosition);
-	color2.g = rand( ( 1.0 / fract (time / 10000.0)) * combinedPosition);
-	color2.b = rand( ( 1.0 / fract (time / 10000.0)) * combinedPosition);
+	vec3 noise = vec3 (rand(combinedPosition/time),rand(combinedPosition/time),rand(combinedPosition/time));
 
 	
 
 	//Anchoring to color; this allows for Pure Intent to be properly reflected.
-	color2 *=  color;
+	//color2 /=  color;
 
 	
 
 	//This variable is supposed to display the end result should this shader be
 	//successful. Its starting state is that of a sine wave signal; the point is to
 	//try and mimic how a biological nervous system works.
-	vec3 color3 = vec3(sinSig, sinSig, sinSig);
+	vec3 color; //= vec3(sinSig, sinSig, sinSig);
 
 	
 	//This is the actual inner workings of the code, once it works.
@@ -236,12 +124,24 @@ void main( void ) {
 	//to appear on the screen; this amounts to causing an image to appear on
 	//screen, as if we're looking from the perspective of an invisible camera.
 	
+	//float nel1Sum = nel1.r+nel1.g+nel1.b;
+	color = vec3(rand(noise),rand(noise),rand(noise));
+	
+	//color = vec3(rand(noise*nel1),rand(noise*(nel2),rand(noise*nel3)));
+	
+
+	
+	
+	
+	
+	//color3 = normalize(color3);
+	
 	//TODO: actually translate this so that the reality talks NTSC...
-	color3.r =  ((color3.r) * (color3.r/color2.r) * combinedPosition );
+	//color3.r =  ((1./color2.r) * rand(combinedPosition/time) );
 
-	color3.g =  ((color3.g) * (color3.g/color2.g) * combinedPosition );
+	//color3.g =  ((1./color2.g) * rand(combinedPosition*time) );
 
-	color3.b =  ((color3.b) * (color3.b/color2.b) * combinedPosition );
+	//color3.b =  ((1./color2.b) * rand(combinedPosition*time) );
 	
 	
 	//TEST: NTSC algorithm is enough...?
@@ -249,7 +149,9 @@ void main( void ) {
 	
 	//TEST: Let's see if this works...
 	
-	color3 = mix(color3.rgb, NTSCtoSRGB(color3.rgb), 1.0);
+	//color3 = DecodeGamma (color3, 3.4);
+	
+	//color3 = mix(color3.rgb, NTSCtoSRGB(color3.rgb), 1.0);
 	
 	//color3 = normalize(color3);
 
@@ -276,7 +178,7 @@ void main( void ) {
 
 	
 	//The finisher: display the results...
-	gl_FragColor = vec4( color3, 1.0 );
+	gl_FragColor = vec4( nel, 1.0 );
 
 	//gl_FragColor = vec4 ( 1.0,1.0,1.0,1.0); //To prevent seizures when needed...
 
